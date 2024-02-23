ddb4a11968edac6d19d9a94b3abe553515d6e923
Ubuntu <ubuntu@ip-172-31-27-251.us-east-2.compute.internal>
2024-02-23 03:27:36 +0000

    we're so fucking back

--- workers/dns_worker.py
+++ workers/dns_worker.py
@@ -3,11 +3,17 @@ import json
 import ray
 import os
 import time
-from brain.db_processor import get_user_id_from_hunter_id, insert_dns_data_remote
+from brain.db_processor import get_user_id_from_hunter_id, insert_dns_data_remote, execute_db_query 
 from brain.base_worker import BaseWorker
 
 @ray.remote
 class DNSWorker(BaseWorker):
+    def check_subdomain_id_exists(self, subdomain):
+        query = "SELECT subdomain_id FROM subdomains WHERE subdomain = %s"
+        subdomain_id = execute_db_query(query, (subdomain,), fetch_one=True)
+        return subdomain_id is not None
+
+
     def process_task(self, task_data):
         _, task = task_data
         subdomain = task['subdomain']
@@ -18,6 +24,16 @@ class DNSWorker(BaseWorker):
         dnsx_cmd = [dnsx_path, '-silent', '-t', '200', '-json', '-asn', '-wd', root_domain,
                     '-rcode', 'noerror,servfail,refused,nxdomain', 
                     '-r', '8.8.8.8,8.8.4.4,1.1.1.1,9.9.9.9,208.67.222.222,84.200.69.80,64.6.64.6,8.26.56.26,205.171.3.65,134.195.4.2,185.222.222.8.9,76.76.19.19,37.235.1.177,77.88.8.1,94.140.14.140,38.132.106.139,74.82.42.42,76.76.2.0']
+        max_retries = 5
+        retry_count = 0
+        while not self.check_subdomain_id_exists(subdomain) and retry_count < max_retries:
+            print(f"Waiting for subdomain ID for {subdomain} to be available...")
+            time.sleep(2)  # Wait for 2 seconds before retrying
+            retry_count += 1
+
+        if retry_count == max_retries:
+            print(f"Subdomain ID for {subdomain} not found after retries, skipping DNS processing.")
+            return             
 
         try:
             process = subprocess.Popen(dnsx_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
@@ -44,6 +60,8 @@ class DNSWorker(BaseWorker):
 
         except Exception as e:
             print(f"Error processing DNS for {subdomain}: {e}")
+
+        
 
     def run(self):
         try:
