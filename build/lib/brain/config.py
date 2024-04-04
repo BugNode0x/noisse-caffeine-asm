0529d259fe3e6ee1e38d9bb3ea0382bf915e5ad3
Sam Paredes <sam@bugnode.io>
2024-04-04 22:54:56 +0000

    hurls implemented

---
+++ build/lib/brain/config.py
@@ -0,0 +1,16 @@
+# Redis Configuration
+REDIS_HOST = 'monorail.proxy.rlwy.net'
+REDIS_PORT = 48372
+REDIS_PWD = '4po1MN2FN4bpgChho3GmbCL22njfIiGj'
+
+
+# PostgreSQL Configuration
+POSTGRES_HOST = 'viaduct.proxy.rlwy.net'
+POSTGRES_PORT = 42055
+POSTGRES_DB = 'railway'
+POSTGRES_USER = 'postgres'
+POSTGRES_PASSWORD = 'E15GA-aA*g*B5D6cfb6fdAg1abBb3G45'
+
+# Admin Webhook 
+
+ADMIN_WEBHOOK = 'https://hooks.slack.com/services/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'
